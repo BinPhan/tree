f68239e1a65410fc298390c8e7c8740ee18141f9
BinPhan <binalwayssmile@gmail.com>
2022-11-14 17:52:44 +0700

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'tree';
+
+  constructor() {
+
+  }
+
+  @ViewChild('mainCanvas', { static: false }) mainCanvas: ElementRef
+
+  public context: CanvasRenderingContext2D;
+
+  ngAfterViewInit(): void {
+    this.context = this.myCanvas.nativeElement.getContext('2d');
+  }
 }
