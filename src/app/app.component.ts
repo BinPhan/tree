387a4b881b253a9c9a9b71e427839d56c4854d60
BinPhan <binalwayssmile@gmail.com>
2022-11-20 19:36:15 +0700

    uPDATE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,59 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 export class AppComponent implements AfterViewInit {
   title = 'tree';
 
+  data: Array<any> = [
+    {
+      id: 1,
+      name: "Gramps",
+      childrens: [
+        {
+          name: "Son"
+        },
+        {
+          name: "Daughter"
+        }
+      ],
+      mother: {
+
+      },
+      father: {
+
+      },
+      gender: 0, // 0 = male, 1 = female
+      SO: {
+        id: 2,
+        name: "Maam"
+      }
+    }
+  ];
+
   constructor() {
 
   }
 
-  @ViewChild('mainCanvas', { static: false }) mainCanvas: ElementRef
+  @ViewChild('mainCanvas', { static: false }) mainCanvas!: ElementRef
 
-  public context: CanvasRenderingContext2D;
+  public context!: CanvasRenderingContext2D;
 
   ngAfterViewInit(): void {
-    this.context = this.myCanvas.nativeElement.getContext('2d');
+
+    this.mainCanvas.nativeElement.width = window.innerWidth
+    this.mainCanvas.nativeElement.height = window.innerHeight
+
+    this.mainCanvas.nativeElement.addEventListener('mousedown', (e: any) => {
+      console.log(e);
+    })
+    this.context = this.mainCanvas.nativeElement.getContext('2d');
+
+    console.log(this.context);
+
+    this.context.beginPath();
+    this.context.rect(20, 20, 150, 100);
+    this.context.stroke();
+    this.context.fillText(this.data[0].name, 30, 30)
+  }
+
+  drawAPerson(person: any) {
+
   }
 }
